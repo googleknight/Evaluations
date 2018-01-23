d7e1cfe25e5150dc7027c09e7999cbab04555d12
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-23 11:12:48 +0530

    Without checking spare and strike base condition passes

--- evaluation1.js
+++ evaluation1.js
@@ -1,5 +1,21 @@
-function score(inputArray) {
-
+function checkOpenframe(i, inputArray) {
+  return (i + 1) < inputArray.length && inputArray[i] + inputArray[i + 1] < 10;
 }
 
-module.exports = score;
+function calculateScore(inputArray) {
+  let result = 0;
+  // const prev = inputArray[0];
+  // const scoreOfFrame = [];
+  for (let i = 0; i < inputArray.length; i += 1) {
+    if (inputArray[i] < 10) {
+      if (checkOpenframe(i, inputArray)) {
+        result += inputArray[i] + inputArray[i + 1];
+        i += 1;
+      }
+    }
+  }
+  return result;
+}
+
+
+module.exports = calculateScore;
--- evaluation1.test.js
+++ evaluation1.test.js
@@ -1,8 +1,8 @@
-const score = require('./evaluation1');
+const calculateScore = require('./evaluation1');
 
 describe('General direct test cases for scoring ', () => {
   it('input without spare or strike', () => {
-    expect(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+    expect(calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
   });
 });
 
