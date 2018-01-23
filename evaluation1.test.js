cc8dafc8f2d9ba99c4ea35b30c8a5a17a5371d7f
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-23 11:59:26 +0530

    consecutive spares tested

--- evaluation1.test.js
+++ evaluation1.test.js
@@ -7,6 +7,12 @@ describe('General direct test cases for scoring ', () => {
   it('input with 1 spare no strike', () => {
     expect(calculateScore([3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(94);
   });
+  it('input with 2 consecuitve spares no strike', () => {
+    expect(calculateScore([3, 7, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(98);
+  });
+  it('input with consecuitve spares no strike', () => {
+    expect(calculateScore([3, 7, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(98);
+  });
 });
 
 // describe('Testing checking functions ', () => {
