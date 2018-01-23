2b85c98bb6f337339d8be53e686da6349477bc72
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-23 11:56:32 +0530

    Tested for single spare in between

--- evaluation1.js
+++ evaluation1.js
@@ -1,17 +1,24 @@
+function checkStrike(pins) {
+  return pins === 10;
+}
+function checkSpare(i, inputArray) {
+  return (i + 1) < inputArray.length && inputArray[i] + inputArray[i + 1] === 10;
+}
 function checkOpenframe(i, inputArray) {
   return (i + 1) < inputArray.length && inputArray[i] + inputArray[i + 1] < 10;
 }
 
 function calculateScore(inputArray) {
   let result = 0;
-  // const prev = inputArray[0];
-  // const scoreOfFrame = [];
-  for (let i = 0; i < inputArray.length; i += 1) {
-    if (inputArray[i] < 10) {
-      if (checkOpenframe(i, inputArray)) {
-        result += inputArray[i] + inputArray[i + 1];
-        i += 1;
-      }
+  for (let i = 0; i < inputArray.length - 1; i += 1) {
+    if (checkStrike(inputArray[i])) {
+      result += 10 + inputArray[i + 1] + inputArray[i + 2];
+    } else if (checkSpare(i, inputArray)) {
+      result += 10 + inputArray[i + 2];
+      i += 1;
+    } else if (checkOpenframe(i, inputArray)) {
+      result += inputArray[i] + inputArray[i + 1];
+      i += 1;
     }
   }
   return result;
--- evaluation1.test.js
+++ evaluation1.test.js
@@ -4,5 +4,16 @@ describe('General direct test cases for scoring ', () => {
   it('input without spare or strike', () => {
     expect(calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
   });
+  it('input with 1 spare no strike', () => {
+    expect(calculateScore([3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(94);
+  });
 });
 
+// describe('Testing checking functions ', () => {
+//   it('checking spare, should return false', () => {
+//     expect(calculateScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+//   });
+//   it('input with 1 spare no strike', () => {
+//     expect(calculateScore([3, 6, 3, 7, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(94);
+//   });
+// });
